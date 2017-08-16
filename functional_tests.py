85f3e24a15112bf99a1e607162a53e61f8302a50
sethgerou <seth.gerou@gmail.com>
2017-08-15 17:47:41 -0700

    update functional tests to test for header text, text input, and presence of to-do items

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisiortTest(unittest.TestCase):
@@ -14,15 +16,24 @@ class NewVisiortTest(unittest.TestCase):
 
         # look for To-Do in page title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # user can add a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # user types in "go grocery shopping"
-
+        inputbox.send_keys('go grocery shopping')
         # when user hits enter, page updates and lists "1. go grocery shopping" is shown in the list.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: go grocery shopping' for row in rows))
         # the text box to add an item is still present.  User enters "pressure wash driveway".
+        self.fail('Finish the test!')
 
         # page updates again and both items are present.
 
@@ -32,4 +43,3 @@ class NewVisiortTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-    
