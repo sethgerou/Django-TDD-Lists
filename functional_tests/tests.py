4f5a22288a77199ab641309e0faaa7ab59e31c1b
sethgerou <seth.gerou@gmail.com>
2017-08-18 09:11:07 -0700

    refactor functional tests using while loop to remove voodoo sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,19 +1,32 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
 class NewVisiortTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+
+        MAX_WAIT = 10
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn('row_text', [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # look at served page
@@ -32,8 +45,7 @@ class NewVisiortTest(LiveServerTestCase):
         inputbox.send_keys('go grocery shopping')
         # when user hits enter, page updates and lists "1. go grocery shopping" is shown in the list.
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: go grocery shopping')
+        self.wait_for_row_in_list_table('1: go grocery shopping')
 
         # the text box to add an item is still present.  User enters "pressure wash driveway".
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -42,8 +54,8 @@ class NewVisiortTest(LiveServerTestCase):
         time.sleep(1)
 
         # page updates again and both items are present.
-        self.check_for_row_in_list_table('1: go grocery shopping')
-        self.check_for_row_in_list_table('2: pressure wash driveway')
+        self.wait_for_row_in_list_table('1: go grocery shopping')
+        self.wait_for_row_in_list_table('2: pressure wash driveway')
 
         # site has generated a unique url and informed user.
         self.fail('Finish the test!')
